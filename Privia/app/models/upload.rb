ea6279967fb1bad3a208583f624d123c7dc61087
Coder Parth <coderparth@Parths-MacBook-Air.local>
2016-05-17 11:03:53 -0500

    added migrations and models for customer and upload classes

---
+++ Privia/app/models/upload.rb
@@ -0,0 +1,3 @@
+class Upload < ActiveRecord::Base
+	has_many :purchases
+end
--- Privia/db/migrate/20160517044635_create_purchases.rb
+++ Privia/db/migrate/20160517044635_create_purchases.rb
@@ -1,13 +1,16 @@
 class CreatePurchases < ActiveRecord::Migration
   def change
     create_table :purchases do |t|
-      t.string :customer_name, null: false
       t.integer :quantity, null: false
       t.integer :item_id, null: false
       t.integer :merchant_id, null: false
+      t.integer :customer_id, null: false
+      t.integer :upload_id
       t.timestamps null: false
     end
     add_foreign_key :purchases, :items
     add_foreign_key :purchases, :merchants
+    add_foreign_key :purchases, :customers
+    add_foreign_key :purchases, :uploads
   end
 end
