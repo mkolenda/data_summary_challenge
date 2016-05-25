7fc399c21c4f9252ba70ee6ac67744927ac5be10
Alison Tague <alisonmtague@gmail.com>
2016-05-25 17:26:44 -0400

    Created import functions

--- privia_challenge/app/controllers/purchases_controller.rb
+++ privia_challenge/app/controllers/purchases_controller.rb
@@ -1,4 +1,9 @@
 class PurchasesController < ApplicationController
   def index
   end
+
+  def import
+    Purchase.import(params[:file])
+    redirect_to root_url, notice: "Purchases file imported."
+  end
 end
--- privia_challenge/app/models/purchase.rb
+++ privia_challenge/app/models/purchase.rb
@@ -1,2 +1,10 @@
 class Purchase < ActiveRecord::Base
+  def self.import(file)
+    CSV.foreach(file.path, :headers => true,
+                           :header_converters => lambda { |h| h.downcase.gsub(' ', '_')},
+                           :col_sep => "\t"
+                           ) do |row|
+      Purchase.create! row.to_hash
+    end
+  end
 end
