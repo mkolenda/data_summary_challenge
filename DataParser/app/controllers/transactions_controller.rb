66a3228598e4e650d3b4d1b2fb5b01cb8ec9cc7c
Chris Mark <chdmark@gmail.com>
2016-05-27 16:39:31 -0700

    Add index and create route for transactions controller

--- DataParser/app/controllers/transactions_controller.rb
+++ DataParser/app/controllers/transactions_controller.rb
@@ -1,2 +1,10 @@
 class TransactionsController < ApplicationController
+
+	def index
+
+	end
+
+	def create
+		@gross_total = Transaction.save_information(params[:transaction][:attachment])
+	end
 end
