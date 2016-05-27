9af8c974e5d7c8ebda7e8054ab82594e6bab9824
Chris Mark <chdmark@gmail.com>
2016-05-27 15:58:07 -0700

    Create products table

--- DataParser/db/migrate/20160527225353_create_products.rb
+++ DataParser/db/migrate/20160527225353_create_products.rb
@@ -1,8 +1,9 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
-      t.timestamps null: false
+    	t.string :description, null: false
+    	t.float :price, null: false
+    	t.timestamps null: false
     end
   end
 end
