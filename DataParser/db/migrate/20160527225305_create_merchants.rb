28b112d23a71bfcc67a1e9a23a7fc4b8df7a3f17
Chris Mark <chdmark@gmail.com>
2016-05-27 15:56:58 -0700

    Create merchant table

--- DataParser/db/migrate/20160527225305_create_merchants.rb
+++ DataParser/db/migrate/20160527225305_create_merchants.rb
@@ -1,8 +1,9 @@
 class CreateMerchants < ActiveRecord::Migration
   def change
     create_table :merchants do |t|
-
-      t.timestamps null: false
+    	t.string :name, null: false
+    	t.string :address, null: false
+		t.timestamps null: false
     end
   end
 end
