3855f3d85a7a24c7623f0299400574237a0e5e63
Chris Mark <chdmark@gmail.com>
2016-05-27 16:14:05 -0700

    Add model test for customer

--- DataParser/spec/models/customer_spec.rb
+++ DataParser/spec/models/customer_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:good_customer) {FactoryGirl.build :customer}
+  let(:no_name) {FactoryGirl.build :customer, name: ""}
+	describe 'validations' do 
+		context 'will raise an error' do 
+			it 'when there is no name' do 
+				no_name.save
+				expect(no_name.errors[:name]).to eq(["can't be blank"])
+			end
+		end
+	end
+	it "successfully saves on a good user" do 
+		expect{good_customer.save}.to change{Customer.count}.by(1)
+	end
 end
