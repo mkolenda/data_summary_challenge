39239e51eb96459066639d838cf2c0b61c7cf18b
Alison Tague <alisonmtague@gmail.com>
2016-06-01 12:07:20 -0400

    Creating tests

--- privia_challenge/app/models/purchase.rb
+++ privia_challenge/app/models/purchase.rb
@@ -1,4 +1,11 @@
 class Purchase < ActiveRecord::Base
+  validates_presence_of :purchaser_name,
+                        :item_description, 
+                        :item_price,
+                        :purchase_count,
+                        :merchant_address,
+                        :merchant_name
+
   def self.import(file)
     CSV.foreach(file.path, :headers => true,
                            :header_converters => lambda { |h| h.downcase.gsub(' ', '_')},
--- privia_challenge/test/controllers/purchases_controller_test.rb
+++ privia_challenge/test/controllers/purchases_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class PurchasesControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  # end
+
+  # def test_purchaser_name:string
+  #   get :purchaser_name:string
+  #   assert_response :success
+  # end
 
 end
